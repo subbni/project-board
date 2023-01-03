b95dac9953b3be3d17dd261ff1bb9ffed8879f76
subbni <slfkak@naver.com>
2023-01-04 00:05:12 +0900

    #19- 회원 계정 엔티티와 게시글, 댓글에 연관관계 적용
    하는 김에 게시글 -> 댓글 컬렉션의 정렬을 최신 순으로 적용.

--- src/main/java/com/example/projectboard/domain/ArticleComment.java
+++ src/main/java/com/example/projectboard/domain/ArticleComment.java
@@ -4,21 +4,13 @@ import javax.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
-        @Index(columnList = "createdBy")
 })
 @Entity
 public class ArticleComment extends AuditingFields {
@@ -27,25 +19,31 @@ public class ArticleComment extends AuditingFields {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @Setter
     @ManyToOne(optional = false)
-    private Article article; //해당 게시글
+    private Article article; // 게시글 (ID)
     @Setter
-    @Column(nullable = false, length=500)
-    private String content; //본문
+    @ManyToOne(optional = false)
+    private UserAccount userAccount; // 유저 정보 (ID)
+
+    @Setter
+    @Column(nullable = false, length = 500)
+    private String content; // 본문
 
     protected ArticleComment() {
     }
 
-    private ArticleComment(Article article, String content) {
+
+    private ArticleComment(Article article, UserAccount userAccount, String content) {
         this.article = article;
+        this.userAccount = userAccount;
         this.content = content;
     }
 
-    public static ArticleComment of(Article article, String content) {
-        return new ArticleComment(article,content);
+    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
+        return new ArticleComment(article, userAccount, content);
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
