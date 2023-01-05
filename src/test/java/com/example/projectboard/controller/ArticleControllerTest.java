b7cd7e65ddcd951c5ca392586fd3b84a4150bbc9
subbni <slfkak@naver.com>
2023-01-05 14:14:45 +0900

    #24- 게시글 페이지네이션 구현
    `이전 글`, `다음 글`만 구현하므로 게시판 페이지네이션에 비해 단순.
    하지만 첫 글, 마지막 글은 버튼 비활성화 및 가짜 링크 처리가 필요하여 타임리프 문법이 복잡해졌다.
    마지막 글을 판단하기 위해 총 article 개수가 필요해짐에 따라 
    count 쿼리 사용, 서비스 로직을 추가하였다.

--- src/test/java/com/example/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/example/projectboard/controller/ArticleControllerTest.java
@@ -63,12 +63,14 @@ class ArticleControllerTest {
         then(paginationService).should().getPaginationBarNumbers(anyInt(),anyInt());
     }
 
-    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
+    @DisplayName("[view][GET] 게시글 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
         // Given
         Long articleId = 1L;
+        long totalCount = 1L;
         given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentDto());
+        given(articleService.getArticleCount()).willReturn(totalCount);
 
         // When & Then
         mvc.perform(get("/articles/"+articleId))
@@ -76,9 +78,11 @@ class ArticleControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
-                .andExpect(model().attributeExists("articleComments"));;
+                .andExpect(model().attributeExists("articleComments"))
+                        .andExpect(model().attribute("totalCount",totalCount));
 
         then(articleService).should().getArticle(articleId);
+        then(articleService).should().getArticleCount();
     }
 
     @Disabled("구현 중")
